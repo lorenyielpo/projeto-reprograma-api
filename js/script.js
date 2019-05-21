61bc2ef49f59fc0761361046fde4902e0bf3b872
Loreny Ielpo <lorenyielpo@gmail.com>
2019-05-21 20:31:09 -0300

    Get com os nomes das mulheres

--- js/script.js
+++ js/script.js
@@ -1,16 +1,32 @@
 let maravBox = document.querySelector(".maravilhosas__box");
 
-let maraPerfil = document.createElement("div");
-maraPerfil.setAttribute("class", ".maravilhosas__box");
+fetch('https://theblackwomanhistory.firebaseio.com/.json')
+    .then(response => {
+        return response.json()
+    })
+    .then(woman => {
+        console.log(woman)
+        woman.content.forEach(mulheres => {
+            let maraPerfil = document.createElement("div");
+            maraPerfil.setAttribute("class", "maravilhosas__perfil");
 
-let nomeMara = document.createElement("a");
-nomeMara.setAttribute('href', "#!");
+            let aMara = document.createElement("a");
+            aMara.setAttribute('href', "#!");
 
-let imgMara = document.createElement('img')
-imgMara.setAttribute("class", "img-responsive");
-imgMara.setAttribute("src", x);
-imgMara.setAttribute("alt", "Foto da personalidade");
+            let imgMara = document.createElement('img')
+            imgMara.setAttribute("class", "img-responsive");
+            imgMara.setAttribute("src", "#!");
+            imgMara.setAttribute("alt", "Foto da personalidade");
 
-maravBox.appendChild(maraPerfil);
-maraPerfil.appendChild(nomeMara);
-maraPerfil.appendChild(imgMara)
+            let nomeMara = document.createElement("p");
+            nomeMara.innerHTML = mulheres.title
+
+            maravBox.appendChild(maraPerfil);
+            maraPerfil.appendChild(aMara);
+            aMara.appendChild(imgMara);
+            aMara.appendChild(nomeMara);
+        });
+    })
+    .catch(erro => {
+        console.log(erro)
+    })
