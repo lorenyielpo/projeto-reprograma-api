f791fa03b81d42fad02d4d06b59da61575f11104
Loreny Ielpo <lorenyielpo@gmail.com>
2019-05-23 12:53:19 -0300

    Post, button e reload

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@ let maravBox = document.querySelector(".maravilhosas__box");
 
 fetch('http://localhost:5001/maravilhosas/')
     .then(response => {
-        return response.json()
+        return response.json();
     })
     .then(woman => {
-        console.log(woman)
+        console.log(woman);
 
         woman.content.forEach(mulheres => {
-            console.log(mulheres)
+            console.log(mulheres);
 
             let maraPerfil = document.createElement("div");
             maraPerfil.setAttribute("class", "maravilhosas__perfil");
@@ -23,72 +23,76 @@ fetch('http://localhost:5001/maravilhosas/')
             (mulheres.metadata && mulheres.metadata.image) ? imgMara.setAttribute("src", mulheres.metadata.image.url) : imgMara.setAttribute("src", "img/img-mulher.png");
 
             let nomeMara = document.createElement("p");
-            nomeMara.innerHTML = mulheres.title
+            nomeMara.innerHTML = mulheres.title;
 
             maravBox.appendChild(maraPerfil);
             maraPerfil.appendChild(aMara);
             aMara.appendChild(imgMara);
             aMara.appendChild(nomeMara);
-        });
 
-        const btn = document.querySelector("send-form");
-        btn.addEventListener("click", (evento) => {
-            evento.preventDefault()
-
-            nomeMaravilhosa = document.querySelector('#nomeMara').value;
-            imageLink = document.querySelector('#imageUrl').value;
-
-            fetch('localhost:5001/maravilhosas/', {
-                method: POST,
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    'title': nomeMaravilhosa,
-                    'urlImage': imageLink
-                })
-            })
-                .then(response => {
-                    return response.json();
-                })
-                .then(mulher => {
-                    console.log(mulher)
-                    mulher.content.forEach(mulherCard => {
-                        let maraPerfil = document.createElement("div");
-                        maraPerfil.setAttribute("class", "maravilhosas__perfil");
-            
-                        let aMara = document.createElement("a");
-                        aMara.setAttribute('href', "#!");
-            
-                        let imgMara = document.createElement('img')
-                        imgMara.setAttribute("class", "img-responsive");
-                        imgMara.setAttribute("alt", "Foto da personalidade");
-            
-                        if(mulherCard.metadata && mulherCard.metadata.image) {
-                            imgMara.setAttribute("src", mulheres.metadata.image.url)
-                        } else if(mulher.urlImage){
-                            imgMara.setAttribute("src", imageLink)
-                        }else {
-                            imgMara.setAttribute("src", "img/img-mulher.png")
-                        };
-            
-                        let nomeMara = document.createElement("p");
-                        nomeMara.innerHTML = mulherCard.title
-            
-                        maravBox.appendChild(maraPerfil);
-                        maraPerfil.appendChild(aMara);
-                        aMara.appendChild(imgMara);
-                        aMara.appendChild(nomeMara); 
-                    })
-                })
-                .catch(erro => {
-                    console.log(erro)
-                })
         });
 
     })
     .catch(erro => {
-        console.log(erro)
+        console.log(erro);
     })
 
+const btn = document.querySelector("#send-form");
+btn.addEventListener("click", (evento) => {
+    evento.preventDefault();
+
+    nomeMaravilhosa = document.querySelector('#nomeMara').value;
+    imageLink = document.querySelector('#imageUrl').value;
+
+    fetch('localhost:5001/maravilhosas/', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            'title': nomeMaravilhosa,
+            'urlImage': imageLink
+        })
+    })
+        .then(response => {
+            return response.json();
+        })
+        .then(mulher => {
+            console.log(mulher);
+
+            location.reload();
+
+            mulher.content.forEach(mulherCard => {
+                let perfilMara = document.createElement("div");
+                perfilMara.setAttribute("class", "maravilhosas__perfil");
+
+                let maraA = document.createElement("a");
+                maraA.setAttribute('href', "#!");
+
+                let maraImg = document.createElement('img');
+                maraImg.setAttribute("class", "img-responsive");
+                maraImg.setAttribute("alt", "Foto da personalidade");
+
+                if (mulherCard.metadata && mulherCard.metadata.image) {
+                    maraImg.setAttribute("src", mulheres.metadata.image.url)
+                } else if (mulher.urlImage) {
+                    maraImg.setAttribute("src", imageLink)
+                } else {
+                    maraImg.setAttribute("src", "img/img-mulher.png")
+                };
+
+                let maraNome = document.createElement("p");
+                maraNome.innerHTML = mulherCard.title;
+
+                maravBox.appendChild(perfilMara);
+                perfilMara.appendChild(maraA);
+                maraA.appendChild(maraImg);
+                maraA.appendChild(maraNome);
+            })
+        })
+        .catch(erro => {
+            console.log(erro);
+        })
+
+})
