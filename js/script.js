166a589845cec0a8fa64847b72c0deec25fc9e17
Loreny Ielpo <lorenyielpo@gmail.com>
2019-05-22 22:26:59 -0300

    post

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 let maravBox = document.querySelector(".maravilhosas__box");
 
-fetch('https://theblackwomanhistory.firebaseio.com/.json')
+fetch('http://localhost:5001/maravilhosas/')
     .then(response => {
         return response.json()
     })
@@ -30,9 +30,65 @@ fetch('https://theblackwomanhistory.firebaseio.com/.json')
             aMara.appendChild(imgMara);
             aMara.appendChild(nomeMara);
         });
+
+        const btn = document.querySelector("send-form");
+        btn.addEventListener("click", (evento) => {
+            evento.preventDefault()
+
+            nomeMaravilhosa = document.querySelector('#nomeMara').value;
+            imageLink = document.querySelector('#imageUrl').value;
+
+            fetch('localhost:5001/maravilhosas/', {
+                method: POST,
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    'title': nomeMaravilhosa,
+                    'urlImage': imageLink
+                })
+            })
+                .then(response => {
+                    return response.json();
+                })
+                .then(mulher => {
+                    console.log(mulher)
+                    mulher.content.forEach(mulherCard => {
+                        let maraPerfil = document.createElement("div");
+                        maraPerfil.setAttribute("class", "maravilhosas__perfil");
+            
+                        let aMara = document.createElement("a");
+                        aMara.setAttribute('href', "#!");
+            
+                        let imgMara = document.createElement('img')
+                        imgMara.setAttribute("class", "img-responsive");
+                        imgMara.setAttribute("alt", "Foto da personalidade");
+            
+                        if(mulherCard.metadata && mulherCard.metadata.image) {
+                            imgMara.setAttribute("src", mulheres.metadata.image.url)
+                        } else if(mulher.urlImage){
+                            imgMara.setAttribute("src", imageLink)
+                        }else {
+                            imgMara.setAttribute("src", "img/img-mulher.png")
+                        };
+            
+                        let nomeMara = document.createElement("p");
+                        nomeMara.innerHTML = mulherCard.title
+            
+                        maravBox.appendChild(maraPerfil);
+                        maraPerfil.appendChild(aMara);
+                        aMara.appendChild(imgMara);
+                        aMara.appendChild(nomeMara); 
+                    })
+                })
+                .catch(erro => {
+                    console.log(erro)
+                })
+        });
+
     })
     .catch(erro => {
         console.log(erro)
     })
-
 
